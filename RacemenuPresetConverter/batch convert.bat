af726b93bcb9c6d0d3f6681877f642af73941f4a
nakalevie <alx.fad3@gmail.com>
2025-01-25 21:15:43 +0200

    Update batch convert.bat

--- RacemenuPresetConverter/batch convert.bat
+++ RacemenuPresetConverter/batch convert.bat
@@ -1,9 +1,11 @@
-:: convert and copy RaceMenu presets to BodySlide slider presets ::
+REM convert and copy RaceMenu presets to BodySlide slider presets ::
 
-:: path to RaceMenu presets folder (ex. ..\SKSE\Plugins\StorageUtilData\RaceMenuMorphsCBBE\Presets)
+REM path to RaceMenu presets folder (ex. ..\SKSE\Plugins\StorageUtilData\RaceMenuMorphsCBBE\Presets)
 set "folderPath=C:\Users\fad3\ModOrganizer\Skyrim Special Edition\overwrite\SKSE\Plugins\StorageUtilData\RaceMenuMorphsCBBE\Presets"
+
 REM path to RaceMenu presets folder (ex. ..\CalienteTools\BodySlide\SliderPresets)
 set "outputPath=C:\Users\fad3\ModOrganizer\Skyrim Special Edition\overwrite\CalienteTools\BodySlide\SliderPresets"
+
 REM convert
 for %%f in ("%folderPath%\*.*") do (
 	REM Arguments
@@ -12,6 +14,7 @@ for %%f in ("%folderPath%\*.*") do (
 	REM 3 - (not required) divider(whole number or fraction separated by a dot)
     RacemenuPresetConverter.exe "%folderPath%\%%~nxf" "yes" "1.5"
 )
+
 REM copy
 for %%o in ("exported\*.*") do (
     copy "%%o" "%outputPath%"
